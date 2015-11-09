6b519ffd94402f68eb224445cac7465e086c8762
JGEsteves89 <jose.esteves.mail@gmail.com>
2015-11-09 08:39:56 +0000

    Wheel Detection Implemented on Angle Based Flattening

--- BlankCalculator/AngleBasedFlattening.cs
+++ BlankCalculator/AngleBasedFlattening.cs
@@ -13,16 +13,49 @@ namespace BlankCalculator {
             Vector<double> Solution = Vector<double>.Build.Dense(Triangles.Count * 3);
             
 
-            List<List<int>> Wheel = new List<List<int>>();
+            List<List<int[]>> Wheels = new List<List<int[]>>();
             for (int i = 0; i < Vertices.Count; i++) {
-
+                List<int> TrianglesWithThisVertice = new List<int>();
+                for (int t = 0; t < Triangles.Count; t++) {
+                    if (Triangles[t][0] == i || Triangles[t][1] == i || Triangles[t][2] == i) TrianglesWithThisVertice.Add(t);
+                }
+                int[] SheredEdge = new int[] { 0, 0 };
+                int Last = 0;
+                int Firt = 0;
+                if (Triangles[TrianglesWithThisVertice[0]][0] == i) { SheredEdge = new int[] {i, Triangles[TrianglesWithThisVertice[0]][1] }; }
+                else if( Triangles[TrianglesWithThisVertice[0]][1] == i) { SheredEdge = new int[] {i, Triangles[TrianglesWithThisVertice[0]][2] }; } 
+                else if (Triangles[TrianglesWithThisVertice[0]][2] == i) { SheredEdge = new int[] { i, Triangles[TrianglesWithThisVertice[0]][0] }; }
+                List<int[]> WheelEdges = new List<int[]>();
+                WheelEdges.Add(SheredEdge);
+                do {
+                    int found=-1 ;
+                    for (int t = 0; t < TrianglesWithThisVertice.Count; t++) {
+                        if (t != Last) {
+                            if (SheredEdge.Contains(Triangles[TrianglesWithThisVertice[t]][0]) && SheredEdge.Contains(Triangles[TrianglesWithThisVertice[t]][1])) {
+                                SheredEdge = new int[] { i, Triangles[TrianglesWithThisVertice[t]][2] }; found = t;
+                            } else if (SheredEdge.Contains(Triangles[TrianglesWithThisVertice[t]][1]) && SheredEdge.Contains(Triangles[TrianglesWithThisVertice[t]][2])) {
+                                SheredEdge = new int[] { i, Triangles[TrianglesWithThisVertice[t]][0] }; found = t;
+                            } else if (SheredEdge.Contains(Triangles[TrianglesWithThisVertice[t]][0]) && SheredEdge.Contains(Triangles[TrianglesWithThisVertice[t]][2])) {
+                                SheredEdge = new int[] { i, Triangles[TrianglesWithThisVertice[t]][1] }; found = t;
+                            }
+                        }
+                    }
+                    if (found != -1) {
+                        Last = found;
+                        if (Last == Firt) goto ClosedWeel;
+                        WheelEdges.Add(SheredEdge);
+                    } else goto Continue;
+                } while (true);
+                ClosedWeel:
+                Wheels.Add(WheelEdges);
+                Continue:
+                continue;
             }
 
-
-            Matrix <double> StiffMatrix = Matrix<double>.Build.Dense(Vertices.Count+ Triangles.Count+ Wheel.Count, Triangles.Count * 3);
+            Matrix <double> StiffMatrix = Matrix<double>.Build.Dense(Vertices.Count+ Triangles.Count+ Wheels.Count, Triangles.Count * 3);
 
             //Vertex consistency ∑ei = 2π −∑αi
-            Vector<double> VertexConsistency = Vector<double>.Build.Dense(Vertices.Count + Triangles.Count + Wheel.Count);
+            Vector<double> VertexConsistency = Vector<double>.Build.Dense(Vertices.Count + Triangles.Count + Wheels.Count);
             for (int i = 0; i < Vertices.Count; i++) {
                 Point3D CurrentVertex = new Point3D(Vertices[i]);
                 List<double> Angles = new List<double>();
@@ -49,7 +82,7 @@ namespace BlankCalculator {
             }
 
             //Triangle Consistency eα +eβ +eγ = π − (α + β + γ) 
-            Vector<double> TriangleConsistency = Vector<double>.Build.Dense(Vertices.Count + Triangles.Count + Wheel.Count);
+            Vector<double> TriangleConsistency = Vector<double>.Build.Dense(Vertices.Count + Triangles.Count + Wheels.Count);
             for (int t = 0; t < Triangles.Count; t++) {
                 double[] Angles = new double[] { 0, 0, 0 };
                 for (int i = 0; i < 3; i++) {
@@ -73,6 +106,35 @@ namespace BlankCalculator {
             }
 
             //Wheel Consistency ∑cot(βi) eβi − cot(γi) eγi =∑log(sinγi) − log(sinβi)
+            Vector<double> WheelConsistency = Vector<double>.Build.Dense(Vertices.Count + Triangles.Count + Wheels.Count);
+            for (int t = 0; t < Wheels.Count; t++) {
+                double cotβi = 0;
+                double cotγi = 0;
+                double logSinβi = 0;
+                double logSinγi = 0;
+                double[] Angles = new double[] { 0, 0, 0 };
+                for (int i = 0; i < Wheels[t].Count; i++) {
+                    Point3D vd0, vd1, vd2;
+                    vd0 = new Point3D(Vertices[Wheels[t][i][0]]);
+                    vd1 = new Point3D(Vertices[Wheels[t][i][1]]);
+                    int other = i + 1;
+                    if (other == Wheels[t].Count) other = 0;
+                    vd2 = new Point3D(Vertices[Wheels[t][other][1]]);
+
+                    Line3D lV1, lV2, lOp;
+                    lV1 = new Line3D(vd0, vd1);
+                    lV2 = new Line3D(vd0, vd2);
+                    lOp = new Line3D(vd1, vd2);
+                    cotβi += Acot(Math.Abs(lV2.Direction.AngleTo(lOp.Direction).Radians));
+                    cotγi += Acot(Math.Abs(lV1.Direction.AngleTo(lOp.Direction).Radians));
+
+                    logSinβi += Math.Log(Math.Sin(Math.Abs(lV2.Direction.AngleTo(lOp.Direction).Radians)));
+                    logSinγi += Math.Log(Math.Sin(Math.Abs(lV1.Direction.AngleTo(lOp.Direction).Radians)));
+
+                    StiffMatrix[Vertices.Count + t, t * 3 + i] = 1;
+                }
+                TriangleConsistency[Vertices.Count + t] = Math.PI - Angles.Sum();
+            }
 
 
 
@@ -82,8 +144,7 @@ namespace BlankCalculator {
 
 
 
-
-                double[,] MatrixA = new double[Vertices.Count * 2, Vertices.Count * 2];
+            double[,] MatrixA = new double[Vertices.Count * 2, Vertices.Count * 2];
 
 
             for (int i = 0; i < Vertices.Count; i++) {
@@ -159,5 +220,9 @@ namespace BlankCalculator {
             Console.WriteLine(X);
             return X;
         }
+        public static double Acot(double x) {
+            return x == 0 ? 0 : Math.Atan(1 / x);
+        }
     }
+
 }
